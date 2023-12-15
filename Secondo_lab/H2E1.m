647cc8a68ee309ac0c77ebab66943f6fa2b61ca7
Guglipadella <133744925+Guglipadella@users.noreply.github.com>
2023-12-15 15:53:05 +0100

    Update H2E1.m

--- Secondo_lab/H2E1.m
+++ Secondo_lab/H2E1.m
@@ -7,8 +7,8 @@ x = x(:, 1);
 % calcolo esplicito della DFT, poi ne faccio il modulo quadro per trovare
 % lo spettro di energia
 
-M = 15;                 % durata sottofinestre temporali
-n_fin = floor(length(x) / (M * fc)); 
+M = 0.5;                 % durata in secondi di ogni sottofinestra temporale
+n_fin = floor(length(x) / (fc * M)); 
 % mi devo calcolare il numero di finestre temporali.
 % floor(A) approssima al primo intero più vicino <= ad A. length(x)/fc e'
 % la lunghezza in secondi del file. numCamp = durata/ Tc, con Tc = 1/fc,
@@ -16,7 +16,7 @@ n_fin = floor(length(x) / (M * fc));
 % durata in secondi per la grandezza della finestra per sapere quante
 % finestre ho.
 
-for i = n_fin
+for i = 1 : 1
 % start_fin e end_fin sono gli # esimi campioni in cui comincia/finisce la
 % finestra
     start_fin = (i - 1) * M * fc + 1;
@@ -25,7 +25,8 @@ for i = n_fin
 % mette nel primo campione della finestra corrente
     end_fin = i * M * fc;
 
-    sub_fin = x(start_fin : end_fin);
+    sub_fin = x(start_fin : end_fin)';  % lo traspongo altrimenti viene colonna
+
 % la mia sottofinestra. Qui sotto inizio il calcolo della dft
 % metodo 1: manualmente
 
@@ -33,23 +34,52 @@ for i = n_fin
     X = zeros(1, N);        % qui mettero' la mia dft
 
     for k = 1:N
-        for n = 1:N
-            X(k) = X(k) + x(n) * exp(-1i * 2 * pi * (k-1) * (n-1) / N);
-        end
+        
+            X(k) = sum(sub_fin.* exp((-1i * 2 * pi * (k-1)) .* (0:N-1) / N));
+
+            
+            %disp(['iterazione ' num2str(k) ' di ' num2str(N)]);
+
+        
     end
 
 % lo spettro di energia e' il modulo quadro della dft
-    SE1 = abs(X).^2;
+    SE1 = abs(fftshift(X)).^2;
 
-    %figure
-    %set (gca, 'Fontsize', 14);
-    %stem()
+    figure('Name','1')
+    set(gca, 'FontSize', 19);
+    stem(1:length(X), SE1, 'b');
+    xlabel('k');
+    ylabel('spettro di energia');
+    title('DFT a manina');
+    
+    grid on;
+    
+
+    SE2 = abs(fftshift(fft(sub_fin))).^2;
+    figure('Name','1')
+    set(gca, 'FontSize', 19);
+    stem(1:length(X), SE2, 'r');
+    xlabel('k');
+    ylabel('spettro di energia');
+    title('FFT non a manina');
+    
+    grid on;
 
 
-
-
-
+    diff = abs(SE1 - SE2);
+    figure('Name','1')
+    set(gca, 'FontSize', 19);
+    stem(1:length(X), diff, 'g');
+    xlabel('k');
+    ylabel('differenza spettri');
+    title('grafico errore');
+    
+    grid on;
 
 
 end
+
+
+
 
