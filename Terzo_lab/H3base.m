9430a6c709e7ff9ace79557b92dc1f4494851fd0
beppSpedicato <55913549+beppSpedicato@users.noreply.github.com>
2024-01-22 16:48:53 +0100

    Update H3base.m

--- Terzo_lab/H3base.m
+++ Terzo_lab/H3base.m
@@ -9,29 +9,29 @@ function result = H3base(x, h, Fs, label)
     XFF = fft(x_filtered); % dft uscita
     XFI = fft([x' zeros(length(x_filtered) - length(x), 1)']'); % dft ingresso
     HF = spettro_di_energia(fftshift(XFF ./ XFI));
+    
+    % plot grafici
+    spettro_x = spettro_di_energia(x);
+    spettro_x_filtered = spettro_di_energia(x_filtered);
+    t_f = linspace(0, length(x_filtered)/Fs, length(x_filtered));
 
     figure;
+    subplot(3,1,1);
     plot(linspace(-length(x_filtered)/(2*Fs), length(x_filtered)/(2*Fs), length(x_filtered)), HF);
     title(['Spettro Funzione di trasferimento H(f)' label]);
     xlabel('Frequenza Hz');
     ylabel('Ampiezza');
-    grid on;
     
-    % Plot dello SPETTRO del segnale di input e del segnale filtrato
-    spettro_x = spettro_di_energia(x);
-    spettro_x_filtered = spettro_di_energia(x_filtered);
-    t_f = linspace(0, length(x_filtered)/Fs, length(x_filtered));
-    
-    figure;
-    subplot(2,1,1);
-    plot(t_x, spettro_x);
+    subplot(3,1,2);
+    plot(t_f, [spettro_x' zeros(1, length(x_filtered)-length(x))]' );
     title(['Spettro Segnale di Input' label]);
     xlabel('Tempo (s)');
     ylabel('Ampiezza');
     
-    subplot(2,1,2);
+    subplot(3,1,3);
     plot(t_f, spettro_x_filtered);
     title(['Spettro Segnale Filtrato' label]);
     xlabel('Tempo (s)');
     ylabel('Ampiezza');
+    grid on;
 end
